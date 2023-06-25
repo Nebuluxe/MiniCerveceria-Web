938c054740722cff1fc3a28bdaf815511d869c94
AlexSkyBlue <pandikwa@gmail.com>
2023-06-25 09:06:50 -0400

    Se generan métodos para Carrito de compras (CrearCarritoCompra, EliminarLinea, EliminarCarrito, ObtenerCarritoCompra, AgregarCantidadProducto, DisminuirCantidadProducto)

---
+++ MiniCerveceria/Modelos/CarritoCompra.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiniCerveceria.Modelos
+{
+    public class CarritoCompra
+    {
+        public int id_carrito { get; set; }
+        public int id_usuario { get; set; }
+        public int id_producto { get; set; }
+        public int precio_producto { get; set; }
+        public int nro_linea { get; set; }
+        public int cantidad { get; set; }
+        public int total_detalle { get; set; }
+        public DateTime fecha_creacion { get; set; }
+        public DateTime fecha_modificacion { get; set; }
+    }
+}
---
+++ MiniCerveceria/Servicios/ICarritoCompraAplicacionServicios.cs
@@ -0,0 +1,15 @@
+﻿using MiniCerveceria.Modelos;
+using System.Collections.Generic;
+
+namespace MiniCerveceria.Servicios
+{
+    internal interface ICarritoCompraAplicacionServicios
+    {
+        void CrearCarritoCompra(CarritoCompra carritocompra);
+        void EliminarLinea(int id_usuario, int id_carrito, int id_producto, int nro_linea);
+        void EliminarCarrito(int id_usuario, int id_carrito);
+        IList<CarritoCompra> ObtenerCarritoCompra(int id_usuario);
+        void AgregarCantidadProducto(CarritoCompra carritocompra);
+        void DisminuirCantidadProducto(CarritoCompra carritocompra);
+    }
+}
